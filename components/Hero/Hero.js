3178c907671fad8885a22adb19704cb74e344c25
Ayush bahuguna <ayushbahuguna1122@gmail.com>
2023-12-22 04:20:21 +0000

    hero text animation added

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,6 +1,25 @@
+"use client"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { GithubIcon, Instagram, Linkedin } from "lucide-react";
 
 export default function Hero() {
+    useGSAP(() => {
+
+        gsap.to('.logo', {
+            fontSize: '2rem',
+            top: '10px',
+            left: '10px',
+            scrollTrigger: {
+                trigger: '.hero',
+                start: 'top top',
+                end: "100",
+                scrub: 0.5
+            }
+        })
+
+    }, [])
+
     return (
         <div className='flex flex-col' style={{ paddingLeft: '10%', paddingTop: '10%', minHeight: '100vh', position: 'relative' }}>
             <div className='p-5 gap-4 pt-10' style={{ position: 'absolute', top: '0px', right: '0px', width: '100vw', height: '10px', display: 'flex', alignItems: 'center', justifyContent: 'end' }}>
@@ -10,7 +29,19 @@ export default function Hero() {
             </div>
 
             <span className='text-9xl font-semibold cursor-scale'>Hey,</span>
-            <span className='text-9xl font-semibold cursor-scale'>I&apos;m Ayush</span>
+            <span className='text-9xl font-semibold cursor-scale'>I&apos;m
+
+                <span className="logo text-9xl"
+                    style={{
+                        position: 'fixed', zIndex: '999'
+                    }}>&nbsp;Ayush</span>
+                <span className="text-9xl"
+                    style={{
+                        mixBlendMode: 'none', WebkitTextStroke: '3px Black',
+                        WebkitTextFillColor: 'transparent'
+                    }}>&nbsp;Ayush</span>
+
+            </span>
             <p className='text-5xl font-semibold cursor-scale small' style={{ paddingLeft: '30%', paddingRight: '10%' }}>- a Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, fugiat!</p>
         </div>
     )
--- components/skills/Skills.jsx
+++ components/skills/Skills.jsx
@@ -21,56 +21,26 @@ export default function Skills() {
         { "name": "Node Js", },
 
     ]
+
     return (
-        <div style={{ height: '100vh' }} className='TextAnimationStop'>
-            <p className='text-8xl cursor-scale'>Skills</p>
-            {/* <div className="wrapper">
+        <>
+            <p className='text-8xl cursor-scale w-1/3'>About me</p>
+            <div style={{ minHeight: '100vh', width: '100vw' }} className='TextAnimationStop flex items-center justify-center p-4'>
+                <p className='w-1/3'>
+                    <p className='text-5xl cursor-scale small'>Get to know me</p>
+                    <br />
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus dolore esse illo voluptatum ipsam, eligendi quibusdam hic ipsa minima accusantium commodi corporis quam recusandae odit, similique aspernatur enim cumque distinctio.</p>
 
-                <Card
-                    img='https://icon-library.com/images/html-icon-png/html-icon-png-15.jpg'
-                    link='https://developer.mozilla.org/en-US/docs/Web/HTML'
-                />
-
-                <Card
-                    img='https://icon-library.com/images/css-icon-png/css-icon-png-0.jpg'
-                    link='https://developer.mozilla.org/en-US/docs/Web/CSS'
-                />
-                <Card
-                    img='https://icon-library.com/images/react_1353128.png'
-                    link='https://react.dev/'
-                />
-                <Card
-                    img='https://icon-library.com/images/bootstrap-icon-png/bootstrap-icon-png-10.jpg'
-                    link='https://getbootstrap.com/'
-                />
-                <Card
-                    img='https://icon-library.com/images/nodejs-icon/nodejs-icon-24.jpg'
-                    link='https://nodejs.org/en'
-                />
-                <Card
-                    img='https://icon-library.com/images/no-sql-icon/no-sql-icon-12.jpg'
-                    link='https://www.mongodb.com/'
-                />
-
-                <Card
-                    img='https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png'
-                    link='https://expressjs.com/'
-                />
-                <Card
-                    img='https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/1920px-Nextjs-logo.svg.png'
-                    link='https://nextjs.org/'
-                />
-
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas delectus voluptate eaque modi officia beatae dolores animi quos molestiae porro vitae fugit, nobis ut obcaecati nulla ducimus, maxime tempora consequatur exercitationem, velit aut ex. Veniam architecto, sequi maiores, atque sit fugit ipsum ad possimus quisquam optio nemo laborum quam earum voluptatibus suscipit perspiciatis natus, repudiandae velit. Voluptatibus repellat, nam harum eum quo provident, sed soluta velit dolorum consequatur ad doloremque blanditiis. Exercitationem temporibus eaque aperiam earum reprehenderit quibusdam aspernatur officiis sequi, laudantium maxime officia hic repudiandae magnam, reiciendis dolor numquam at dicta autem facere natus neque possimus qui? Animi tempore dolorum natus quidem commodi, non officia facilis repudiandae laboriosam, consectetur dignissimos, sunt praesentium reiciendis laborum aliquam omnis labore blanditiis. Temporibus totam vitae sunt omnis similique molestiae ut, dolorem numquam amet laudantium fugit dolores eius, hic, eum soluta? Magni pariatur aut dolorem voluptates obcaecati, assumenda maiores repellendus nihil illo omnis aperiam labore quod fugit dolorum expedita quas earum qui libero consectetur sint laudantium reiciendis similique. Ea molestias minima ducimus consectetur fuga amet numquam est, dolorum nobis? Corporis recusandae nulla expedita, vitae iste neque, fuga eaque nesciunt nostrum quia aut impedit? Libero dolorem nostrum veritatis nihil at odio facere consequuntur nisi repudiandae?
-            </div> */}
-            <div className='flex items-center justify-center w-1/2'>
-                {Skills?.map((Skill, index) => {
-                    return (
-                        <Button key={index} variant={'outline'} className="m-1">{Skill.name}</Button>
-                    )
-                })}
+                <div className='flex items-center justify-center flex-wrap w-1/3'>
+                    <p className='text-5xl cursor-scale small'>My skills and tool</p>
+                    {Skills?.map((Skill, index) => {
+                        return (
+                            <Button key={index} variant={'outline'} className="m-2">{Skill.name}</Button>
+                        )
+                    })}
+                </div>
             </div>
-        </div>
+        </>
 
     )
 }
