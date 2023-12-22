1fc9fb2d868245487945d0b900f03c33b0326367
Ayush bahuguna <ayushbahuguna1122@gmail.com>
2023-12-22 06:55:30 +0000

    some changes

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,11 +1,13 @@
 "use client"
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 import { GithubIcon, Instagram, Linkedin } from "lucide-react";
 
 export default function Hero() {
-    useGSAP(() => {
 
+    useGSAP(() => {
+        gsap.registerPlugin(ScrollTrigger);
         gsap.to('.logo', {
             fontSize: '2rem',
             top: '10px',
@@ -13,17 +15,15 @@ export default function Hero() {
             scrollTrigger: {
                 trigger: '.hero',
                 start: 'top top',
-                end: "100",
+                end: "+=200",
                 scrub: 0.5,
-                ease: "power1.inOut",
             }
         })
-
     }, [])
 
     return (
         <div className='flex flex-col' style={{ paddingLeft: '10%', paddingTop: '10%', minHeight: '100vh', position: 'relative' }}>
-            <div className='p-5 gap-4 pt-10' style={{ position: 'absolute', top: '0px', right: '0px', width: '100vw', height: '10px', display: 'flex', alignItems: 'center', justifyContent: 'end' }}>
+            <div className='p-5 gap-4' style={{ position: 'fixed', top: '10px', right: '0px', width: '100vw', height: '10px', display: 'flex', alignItems: 'center', justifyContent: 'end', zIndex: '999' }}>
                 <GithubIcon size={'35px'} strokeWidth={1.3} className='cursor-scale small' />
                 <Instagram size={'35px'} strokeWidth={1.3} className='cursor-scale small' />
                 <Linkedin size={'35px'} strokeWidth={1.3} className='cursor-scale small' />
