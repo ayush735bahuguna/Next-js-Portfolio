f55ad7025e4570bff879999dedd2d49fee02020c
Ayush bahuguna <ayushbahuguna1122@gmail.com>
2024-01-03 09:30:08 +0000

    zindex change background img

--- components/Services/Services.js
+++ components/Services/Services.js
@@ -4,6 +4,7 @@ import { Button } from '../ui/button'
 import Image from 'next/image'
 
 import ServicesImg from '../../asset/home images/Services.svg'
+import Link from 'next/link'
 
 export default function Services() {
     return (
@@ -18,7 +19,9 @@ export default function Services() {
                     <div className='p-1 flex gap-2 m-2 ml-0'> <Accessibility color='blue' /> Website Audit & Accessibility</div>
                     <div className='p-1 flex gap-2 m-2 ml-0'> <Shield color='blue' />Security and privacy</div>
                     <div className='p-1 flex gap-2 m-2 ml-0'> <User color='blue' />User Experience & User Interface Design</div>
-                    <Button className="text-lg flex gap-2 mt-5">Work with me <ArrowRight /> </Button>
+                    <Link href={'#Contact'}>
+                        <Button className="text-lg flex gap-2 mt-5">Work with me <ArrowRight /> </Button>
+                    </Link>
                 </div>
 
                 <Image src={ServicesImg} alt='' style={{ margin: 'auto' }} className='pt-10' />
